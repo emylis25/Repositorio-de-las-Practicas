da0636d63f6dfb57c4ae14c690020d66f3214e56
EmilysFlores25 <2429841@clases.edu.sv>
2025-03-10 10:42:07 -0600

    Creación de la migración y configuración del Porgram.

---
+++ PracticasEmilyDataFirts/Models/CatalogosDbdataFirtsContext.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PracticasEmilyDataFirts.Models;
+
+public partial class CatalogosDbdataFirtsContext : DbContext
+{
+    public CatalogosDbdataFirtsContext()
+    {
+    }
+
+    public CatalogosDbdataFirtsContext(DbContextOptions<CatalogosDbdataFirtsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Categoria> Categorias { get; set; }
+
+    public virtual DbSet<Cliente> Clientes { get; set; }
+
+    public virtual DbSet<Departamento> Departamentos { get; set; }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    public virtual DbSet<Equipo> Equipos { get; set; }
+
+    public virtual DbSet<Estado> Estados { get; set; }
+
+    public virtual DbSet<MetodosDePago> MetodosDePagos { get; set; }
+
+    public virtual DbSet<Moneda> Monedas { get; set; }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    public virtual DbSet<Proveedore> Proveedores { get; set; }
+
+    public virtual DbSet<Proyecto> Proyectos { get; set; }
+
+    public virtual DbSet<Servicio> Servicios { get; set; }
+
+    public virtual DbSet<TiposDeDocumento> TiposDeDocumentos { get; set; }
+
+    public virtual DbSet<UnidadesDeMedidum> UnidadesDeMedida { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=EMYLIS\\SQLEXPRESS;Initial Catalog=CatalogosDBDataFirts;Integrated Security=True;Encrypt=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Categori__3214EC07CA1A5B4A");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Cliente>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Clientes__3214EC077391E8CA");
+
+            entity.Property(e => e.Direccion).HasMaxLength(255);
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Telefono).HasMaxLength(20);
+        });
+
+        modelBuilder.Entity<Departamento>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Departam__3214EC07C2CDB832");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Empleado__3214EC079F98B13A");
+
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Puesto).HasMaxLength(100);
+            entity.Property(e => e.Telefono).HasMaxLength(20);
+        });
+
+        modelBuilder.Entity<Equipo>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Equipos__3214EC07BA3A4C4B");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Estado>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Estados__3214EC071E67A93E");
+
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<MetodosDePago>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__MetodosD__3214EC074EF2434A");
+
+            entity.ToTable("MetodosDePago");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Moneda>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Monedas__3214EC07868A00DA");
+
+            entity.Property(e => e.Nombre).HasMaxLength(50);
+            entity.Property(e => e.Simbolo).HasMaxLength(10);
+        });
+
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Producto__3214EC076D2D8F60");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Precio).HasColumnType("decimal(10, 2)");
+        });
+
+        modelBuilder.Entity<Proveedore>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Proveedo__3214EC076EB9668C");
+
+            entity.Property(e => e.Direccion).HasMaxLength(255);
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Telefono).HasMaxLength(20);
+        });
+
+        modelBuilder.Entity<Proyecto>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Proyecto__3214EC0728052515");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<Servicio>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Servicio__3214EC07EB94E675");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<TiposDeDocumento>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__TiposDeD__3214EC0749D856ED");
+
+            entity.ToTable("TiposDeDocumento");
+
+            entity.Property(e => e.Descripcion).HasMaxLength(255);
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+        });
+
+        modelBuilder.Entity<UnidadesDeMedidum>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Unidades__3214EC0783BF41D6");
+
+            entity.Property(e => e.Abreviatura).HasMaxLength(10);
+            entity.Property(e => e.Nombre).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- PracticasEmilyDataFirts/Program.cs
+++ PracticasEmilyDataFirts/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using PracticasEmilyDataFirts.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<CatalogosDbdataFirtsContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
 
 var app = builder.Build();
 
