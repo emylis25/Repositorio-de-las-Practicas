7e77dc9ae9424aa793184c5d3d6e91a91b6d023a
EmilysFlores25 <2429841@clases.edu.sv>
2025-03-10 09:41:04 -0600

    Agregar archivos de proyecto.

---
+++ PracticasEmilyCodeFirts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticasEmilyCodeFirts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
