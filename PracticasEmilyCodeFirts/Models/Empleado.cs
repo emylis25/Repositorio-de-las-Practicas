029de52d17a27bb64a9792fc695fc8e24ceb1422
EmilysFlores25 <2429841@clases.edu.sv>
2025-03-10 20:30:01 -0600

    Creación de las Clases de Empleado, Alumno y La Clase de MiDBContext

---
+++ PracticasEmilyCodeFirts/Models/Empleado.cs
@@ -0,0 +1,11 @@
+﻿namespace PracticasEmilyCodeFirts.Models
+{
+    public class Empleado {
+        public int  Id { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+        public string DUI { get; set; }
+        public string Dirección { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ PracticasEmilyCodeFirts/Models/MiDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PracticasEmilyCodeFirts.Models
+{
+    public class MiDbContext : DbContext {
+     public MiDbContext (DbContextOptions<MiDbContext> options) : base(options) { }
+       public DbSet<Empleado> Empleados { get; set; }
+       public DbSet<Alumno> Alumnos { get; set; }
+
+    }
+}
