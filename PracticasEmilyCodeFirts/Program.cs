88a47d79bd2b13d2b95d286874e003a71589c53b
EmilysFlores25 <2429841@clases.edu.sv>
2025-03-10 20:33:18 -0600

    Modificación del archivo Program.

--- PracticasEmilyCodeFirts/Program.cs
+++ PracticasEmilyCodeFirts/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using PracticasEmilyCodeFirts.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MiDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
 
 var app = builder.Build();
 
